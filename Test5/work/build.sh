03f3b08178fa5df9db8ae77a5d4b8a618dd3ead1
Romuald Rousseau <romualdrousseau@gmail.com>
2023-02-02 08:33:45 +0800

    New test

--- Test5/work/build.sh
+++ Test5/work/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/opt/lscc/iCEcube2.2020.12/sbt_backend/bin/linux/opt/synpwrap:$LD_LIBRARY_PATH
-export LM_LICENSE_FILE=/opt/lscc/license.dat
+export LM_LICENSE_FILE=/usr/local/licenses/flexlm/license.dat
 export SYNPLIFY_PATH=/opt/lscc/iCEcube2.2020.12/synpbase
 export SBT_DIR=/opt/lscc/iCEcube2.2020.12/sbt_backend
-/opt/lscc/iCEcube2.2020.12/sbt_backend/bin/linux/opt/synpwrap/synpwrap -prj "/home/romuald/Projects/alchitry/Test5/work/alchitry_syn.prj" -log "/home/romuald/Projects/alchitry/Test5/work/icelog.log"
-tclsh "/home/romuald/Projects/alchitry/Test5/work/iCEcube2_flow.tcl"
+/opt/lscc/iCEcube2.2020.12/sbt_backend/bin/linux/opt/synpwrap/synpwrap -prj "/home/romuald/Projects/Alchitry/Test5/work/alchitry_syn.prj"
+tclsh "/home/romuald/Projects/Alchitry/Test5/work/iCEcube2_flow.tcl"
--- Test5/work/iCEcube2_flow.tcl
+++ Test5/work/iCEcube2_flow.tcl
@@ -2,10 +2,10 @@
 
 set device iCE40HX8K-CB132
 set top_module cu_top_0
-set proj_dir /home/romuald/Projects/alchitry/Test5/work
+set proj_dir /home/romuald/Projects/Alchitry/Test5/work
 set output_dir "alchitry_imp"
 set edif_file "cu_top_0"
-set tool_options ":edifparser -y \"/home/romuald/Projects/alchitry/Test5/work/constraint/merged_constraint.pcf \""
+set tool_options ":edifparser -y \"/home/romuald/Projects/Alchitry/Test5/work/constraint/merged_constraint.pcf \""
 set sbt_root $::env(SBT_DIR)
 append sbt_tcl $sbt_root "/tcl/sbt_backend_synpl.tcl"
 source $sbt_tcl
